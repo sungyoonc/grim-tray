d60dc4130cfb76bcb6e8f2e7857861c39e2f2182
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-08-29 17:45:35 +0900

    feat: screen(all output) screenshot

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from subprocess import Popen, PIPE
+from enum import Enum, auto
+
+
+class Mode(Enum):
+    AREA = auto()
+    WINDOW = auto()
+    SCREEN = auto()
+    OUTPUT = auto()
 
 
 class Screenshot:
-    def capture(self, file, area, output):
+    def capture(self, mode, file="-", area="", output=""):
         # TODO: Add save to file with output_mode
-        grim_subprocess = Popen(["grim", "-g", area, file], stdout=PIPE)
+        if mode == Mode.AREA or mode == Mode.WINDOW:
+            grim_subprocess = Popen(["grim", "-g", area, file], stdout=PIPE)
+        elif mode == Mode.SCREEN:
+            grim_subprocess = Popen(["grim", file], stdout=PIPE)
+        elif mode == Mode.OUTPUT:
+            grim_subprocess = Popen(["grim", "-o", output, file], stdout=PIPE)
+        else:
+            return None
         return grim_subprocess
 
     def area(self):
@@ -16,11 +31,12 @@ class Screenshot:
             print("Area selection cancelled")
             return
 
-        image = self.capture("-", geom.strip().decode("utf-8"), "")
+        image = self.capture(Mode.AREA, area=geom.strip().decode("utf-8"))
+        if image == None:
+            return
         copy_subprocess = Popen(
             ["wl-copy", "--type", "image/png"], stdin=image.stdout, stdout=PIPE
         )
-        # TODO: error handling
         image.wait()
         copy_subprocess.wait()
         tray.showMessage("grim-tray", "Screenshot has been copied to clipboard", icon)
@@ -48,7 +64,21 @@ class Screenshot:
             print("Window selection canceled")
             return
 
-        image = self.capture("-", geom.strip().decode("utf-8"), "")
+        image = self.capture(Mode.WINDOW, area=geom.strip().decode("utf-8"))
+        if image == None:
+            return
+        copy_subprocess = Popen(
+            ["wl-copy", "--type", "image/png"], stdin=image.stdout, stdout=PIPE
+        )
+        # TODO: error handling
+        image.wait()
+        copy_subprocess.wait()
+        tray.showMessage("grim-tray", "Screenshot has been copied to clipboard", icon)
+
+    def screen(self):
+        image = self.capture(Mode.SCREEN)
+        if image == None:
+            return
         copy_subprocess = Popen(
             ["wl-copy", "--type", "image/png"], stdin=image.stdout, stdout=PIPE
         )
@@ -76,9 +106,12 @@ option_area = QAction("Area")
 option_area.triggered.connect(screenshot.area)
 option_window = QAction("Window")
 option_window.triggered.connect(screenshot.window)
+option_screen = QAction("Screen")
+option_screen.triggered.connect(screenshot.screen)
 
 menu.addAction(option_area)
 menu.addAction(option_window)
+menu.addAction(option_screen)
 
 # To quit the app
 menu.addSeparator()
